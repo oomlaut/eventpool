fe23e4e50f63df414ff0fae4fdad22387a0816c2
Paul Gueller <paul.gueller@7summitsagency.com>
2013-12-17 11:54:24 -0600

    create basic logic for controller actions

--- app/app.php
+++ app/app.php
@@ -1,1 +1,15 @@
 <?php
+
+$app_loaded=true;
+
+class App {
+	public initialized = false;
+
+	public __construct(){
+		$this->initialized = true;
+	}
+
+	public __toString(){
+		return ($this->initialized) ? "initialized" : "not initialized" ;
+	}
+}
